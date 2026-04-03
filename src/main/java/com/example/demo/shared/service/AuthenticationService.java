7a009a55afc32ebd03108256246b1af4f0d2ba59
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2026-04-03 00:27:03 -0400

    clean de code

--- src/main/java/com/example/demo/shared/service/AuthenticationService.java
+++ src/main/java/com/example/demo/shared/service/AuthenticationService.java
@@ -2,8 +2,6 @@ package com.example.demo.shared.service;
 
 import com.example.demo.shared.dto.AuthRequest;
 import com.example.demo.shared.dto.AuthResponse;
-import com.example.demo.tenant.model.Role;
-import com.example.demo.tenant.model.User;
 import com.example.demo.tenant.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,31 +9,25 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class AuthenticationService {
 
     private final UserRepository repository;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationService(UserRepository repository, JwtService jwtService, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+    public AuthenticationService(UserRepository repository, JwtService jwtService,
+            AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
         this.repository = repository;
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public AuthResponse authenticate(AuthRequest request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
-                        request.getPassword()
-                )
-        );
+                        request.getPassword()));
         var user = repository.findByEmail(request.getEmail())
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(user);
